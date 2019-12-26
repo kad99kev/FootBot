edd3cb0575c9c1137cf2916bbfeeed76a56a7199
Kevlyn Kadamala <43489154+kad99kev@users.noreply.github.com>
2019-12-26 23:35:58 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports ={
-  consumer_key:         'Dc60VAkUeI32wRg9IrK7YJGCk',
-  consumer_secret:      'QdgzYsRAXuDuNELGTL8cIcJ0ySG6jnRLfQHhdC7w1shMxD8LtR',
-  access_token:         '1302899276-g5QUqb8wYSKbdAzvuFWSzF9aAQAt24i135zKAsx',
-  access_token_secret:  'guzzYGk94tgLdoCTVkUUMi08SZoid3NTCUYPbGN49ih6Z',
-}
